8c6d823cd0c44adb0aa4eabfd9e3bef4990e9466
noah <noah.alerm.7e4@itb.cat>
2022-04-25 19:55:35 +0200

    Update

--- src/main/java/com/example/apirestv3/ApiRestv3Application.java
+++ src/main/java/com/example/apirestv3/ApiRestv3Application.java
@@ -2,10 +2,25 @@ package com.example.apirestv3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
+@RestController
 public class ApiRestv3Application {
 
+    @GetMapping
+    public String welcome() {
+        return "Welcome to APIRestv3";
+    }
+
+    @GetMapping("/user")
+    public Principal user(Principal principal) {
+        System.out.println("username: " + principal.getName());
+        return principal;
+    }
     public static void main(String[] args) {
         SpringApplication.run(ApiRestv3Application.class, args);
     }
