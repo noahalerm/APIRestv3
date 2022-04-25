71d5ec2773c70bd0702954b749ef1ab0c29b205b
noah <noah.alerm.7e4@itb.cat>
2022-04-25 18:49:01 +0200

    Initial Commit

---
+++ src/main/java/com/example/apirestv3/ApiRestv3Application.java
@@ -0,0 +1,13 @@
+package com.example.apirestv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestv3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestv3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apirestv3/ApiRestv3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apirestv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestv3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
